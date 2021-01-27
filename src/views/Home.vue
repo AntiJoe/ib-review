67844912733cafb0819aa6bd3f40bd8a803729c6
AntiJoe <jrallen.automation@gmail.com>
2021-01-26 22:49:02 -0400

    updated Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <whats-my-ip class="whats-my-ip"
+    v-if="true"
+    @api-path="setPath($event)"></whats-my-ip>
+
+    <app-header title="Stock Summary"></app-header>
+
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import AppHeader from '@/components/AppHeader.vue';
+import WhatsMyIp from '@/components/WhatsMyIp.vue';
 
 export default {
+  data () {
+    return {
+      path: "black:3000"
+    }
+
+  },
+  methods: {
+    setPath(path) {
+      this.path = path;
+    }
+
+
+  },
   name: "Home",
   components: {
-    HelloWorld
+    AppHeader,
+    WhatsMyIp
+    // HelloWorld
   }
 };
 </script>
